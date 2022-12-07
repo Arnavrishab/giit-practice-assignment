b0129110e9f2e19dcbefeaafcb6e4ef29119a555
Abhirup Bakshi <abhirup.bakshi@disroot.org>
2022-12-07 20:58:39 +0530

    fixed by m2

--- primeproblem.js
+++ primeproblem.js
@@ -1,13 +1,21 @@
 let count=0;
 let num=5
-   for(let i=2 ; i<num ; i++){
-       if(num%i==0){
-           count++
-          break
-       }
+
+if(num <= 1)
+{
+    console.log("No");
+    return;
+}
+
+for(let i=2; i <= num**0.5 ; i++){
+   if(num % i == 0)
+   {
+      count++;
+      break;
    }
-   if(count==0){
-       console.log("No")
-   }else{
-       console.log("Yes")
-   }
+}
+if(count==0){
+   console.log("Yes");
+}else{
+   console.log("No");
+}
